8a08f954c61e97b0695c9d9c8f0172c95033c966
Last Whisper <xxzworkmail@gmail.com>
2021-01-21 23:17:01 +0800

    初始化样式头部组件完成

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="header">
+    <h1 class="text">Nice Music</h1>
+    <router-link tag="div" class="mine" to="/user">
+      <i class="icon-mine"></i>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "../assets/stylus/variable"
+@import "../assets/stylus/mixin"
+  .header
+    position relative
+    height 44px
+    text-align center
+    color $color-theme
+    font-size 0
+    .text
+      display: inline-block
+      vertical-align: top
+      line-height: 44px
+      font-size: $font-size-large
+    .mine
+      position: absolute
+      top: 0
+      right: 0
+      .icon-mine
+        display: block
+        padding: 12px
+        font-size: 20px
+        color: $color-theme
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import './assets/stylus/index.styl'
+
+// 解决移动端派发点击事件存在300ms延迟
+import fastclick from 'fastclick'
+fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 new Vue({
